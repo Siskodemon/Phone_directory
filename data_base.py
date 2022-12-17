34a870e1a5c10f28de3a558e4625ee8cb633be8f
Siskodemon <whiteprist@gmail.com>
2022-12-17 15:27:03 +0300

    Add, View, Delet, Update BD

--- data_base.py
+++ data_base.py
@@ -6,41 +6,42 @@ def create_connection(path):
     connection = None
     try:
         connection = sqlite3.connect(path)
-        print("Connection to SQLite DB successful")
+        print("Соединение с бд установлено")
     except Error as e:
         print(f"The error '{e}' occurred")
 
     return connection
 
 # Создание таблиц в инициализированной бд
-def create_table(connection, query):
+def create_table(connection, query,name_table):
     cursor = connection.cursor()
     try:
         cursor.execute(query)
         connection.commit()
-        print("Query executed successfully")
+        print(f"Таблица {name_table} успешно создана")
     except Error as e:
         print(f"The error '{e}' occurred")
 
-bd = create_connection('test.db')
+bd = create_connection('direkt.db')
 
 # Добавление данных в таблицу в инициализированной бд
-def insert_in_table(connection, query):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        connection.commit()
-        print("Query executed successfully")
-    except Error as e:
-        print(f"The error '{e}' occurred")
+# def insert_in_table(connection, query):
+#     cursor = connection.cursor()
+#     try:
+#         cursor.execute(query)
+#         connection.commit()
+#         print("Данные успешно внесены")
+#     except Error as e:
+#         print(f"The error '{e}' occurred")
 
 # команда для создания таблицы user в базе данных
 user_table ="""
       CREATE TABLE IF NOT EXISTS users(
       id INTEGER PRIMARY KEY AUTOINCREMENT,
-      patronymic TEXT NOT NULL,
+      patronymic TEXT,
       name TEXT NOT NULL,
       soname TEXT NOT NULL,
+      nikname TEXT NOT NULL,
       age INTEGER,
       gender TEXT,
       nationality TEXT
@@ -50,32 +51,80 @@ user_table ="""
 phone_table = """
       CREATE TABLE IF NOT EXISTS phone(
       id INTEGER PRIMARY KEY AUTOINCREMENT,
-      phone integer NOT NULL,
-      commit TEXT NOT NULL
+      phone INTEGER NOT NULL,
+      comment TEXT,
       user_id INTEGER NOT NULL, 
-      FOREIGN KEY (user_id) REFERENCES users (id)
+      FOREIGN KEY (user_id) REFERENCES users(id)
       );"""
 
-create_table(bd,user_table)
-create_table(bd,phone_table)
+create_table(bd, user_table, 'users')
+create_table(bd, phone_table, 'phone')
 
+#Внесение данных пользователя
 insert=[]
-soname = input('Введите имя - ')
-name = input('Введите фамилию - ')
+
+soname = input('Введите фамилию - ')
+insert.append(soname)
+
+name = input('Введите имя - ')
+insert.append(name)
+
 patronymic = input('Введите отчество - ')
+insert.append(patronymic)
+
+nikname = input('Введите Ник - ')
+insert.append(nikname)
+
+print(f' ФИО + Ник - {insert}')
 
 
-    # if choice == 1:
-    #     name = input("Name\n> ")
-    #     surname = input("Surname\n> ")
-    #     cur.execute(f"INSERT INTO `test` VALUES ('{name}', '{surname}')")
-    # elif choice == 2:
-    #     cur.execute("SELECT * FROM `test`")
-    #     rows = cur.fetchall()
-    #     for row in rows:
-    #         print(row[0], row[1])
-    # else:
-    #     print("Вы ошиблись")
+# #Добавление записей в таблшицу users
+bd1 = bd.cursor()
+bd1.execute(f"""
+INSERT INTO 
+users 
+      (patronymic, name, soname, nikname) 
+VALUES 
+      ('{insert[0]}', '{insert[1]}', '{insert[2]}', '{insert[3]}')""")
 
+# Удаление записей из таблицы users
+# bd1 = bd.cursor()
+# bd1.execute(f"""
+# DELETE FROM
+#       users
+# WHERE
+#       id = 2
+# """)
+
+# Обновление(изменение) записей из таблицы users
+# bd1 = bd.cursor()
+# bd1.execute(f"""
+# UPDATE 
+#      users
+# SET 
+#      age = 30,gender = 'М'
+# WHERE
+#      id = 2
+# """)
+
+bd1 = bd.cursor()
+bd1.execute("SELECT * FROM users")
+id = bd1.fetchall()
+print(f'Тип - {type(id)}')
+n=1
+for i in id:
+    print(i)
+
+#bd1 = bd.cursor()
+bd1.execute("SELECT id FROM users")
+id = bd1.fetchall()
+print(f'Тип - {type(id)}')
+#for i in id:
+print(id[0])
+#         rows = cur.fetchall()
+#         for row in rows:
+#             print(row[0], row[1])
+#     else:
+#         print("Вы ошиблись")
 bd.commit()
 bd.close()
